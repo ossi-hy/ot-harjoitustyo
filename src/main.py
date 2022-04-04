21ed4cd89ef13ca6ceb98b6ad5a01799c0ce2770
Ossi <101654124+ossi-hy@users.noreply.github.com>
2022-04-04 18:54:24 +0300

    added basic renderer

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+from board import Board
+from render import Renderer
+import tkinter as tk
+
 def main():
-    print("Hello world!")
+    gameboard = Board()
+
+    window = tk.Tk()
+    window.title("Tetris")
+
+    renderer = Renderer(window, gameboard)
+    renderer.draw()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
---
+++ src/render.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+from board import Board
+
+COLORS = [
+    (0, 0, 0),
+    (180, 0, 255),
+    (0, 150, 0),
+    (255, 0, 0),
+    (0, 0, 255),
+    (255, 120, 0),
+    (0, 220, 220),
+    (255, 255, 0),
+]
+
+
+class Renderer:
+    def __init__(self, window: tk.Tk, board: Board, width=300, height=600) -> None:
+        self._window = window
+        self.w = width
+        self.h = height
+        self._canvas = tk.Canvas(self._window, width=width, height=height)
+        self._canvas.pack()
+        self._board = board
+
+    def draw(self) -> None:
+        self._canvas.delete("all")
+
+        for y in range(self._board.h):
+            for x in range(self._board.w):
+                color = "#{:02x}{:02x}{:02x}".format(*COLORS[self._board.board[y, x]])
+                self._canvas.create_rectangle(
+                    x * self.w / self._board.w,
+                    y * self.h / self._board.h,
+                    (x + 1) * self.w / self._board.w,
+                    (y + 1) * self.h / self._board.h,
+                    outline="#000000",
+                    stipple="gray25",
+                    fill=color
+                )
