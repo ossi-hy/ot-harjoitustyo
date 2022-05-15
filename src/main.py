fb7aa0eb76a7c5e96bff971018fd178ff5fa2325
Ossi <101654124+ossi-hy@users.noreply.github.com>
2022-05-15 14:51:56 +0300

    renamed time

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import tkinter as tk
-import time
+#import time
+from time import perf_counter as time
 from game.board import Board
 from ui.render import Renderer
 from ui.inputhandler import InputHandler
@@ -17,19 +18,19 @@ def main():
 
     renderer = Renderer(window, gameboard, inputhandler)
 
-    start_time = time.perf_counter()
-    input_time = time.perf_counter()
+    start_time = time()
+    input_time = time()
     while True:
-        inputhandler.process_inputs(renderer, time.perf_counter() - input_time)
-        input_time = time.perf_counter()
+        inputhandler.process_inputs(renderer, time() - input_time)
+        input_time = time()
         if not renderer.draw():
             break
 
-        frametime = time.perf_counter() - start_time
+        frametime = time() - start_time
         # Busyloop wait to keep 60fps
         while frametime < 1 / 60:
-            frametime = time.perf_counter() - start_time
-        start_time = time.perf_counter()
+            frametime = time() - start_time
+        start_time = time()
     window.quit()
 
 
